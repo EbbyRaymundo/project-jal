ecf54f62dd0f91dbcc1b2e3076b68ecbb965423f
GeckoVerde <wwillis4@uncc.edu>
2022-10-18 22:44:28 -0400

    Start Method and Packages
    Start Method, Default Packages, and JRE System Library Added

---
+++ src/Start.java
@@ -0,0 +1,9 @@
+//Inherits all other classes , will be main class
+public class Start {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Testing,,,, testingtestingtestingtestingtestingtestingtesting testingggg");
+
+	}
+
+}
